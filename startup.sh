c9463af305e2f74c3dfc1c9bfeb252bcc62441bd
Dhruv Sharma <dhruvsh5467@gmail.com>
2025-04-26 00:51:20 +0530

    azure fixes

--- startup.sh
+++ startup.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
+
+# Make sure we're in the right directory
 cd /home/site/wwwroot
-gunicorn --config gunicorn_config.py app:app 
+
+# Make sure Python can find the modules
+export PYTHONPATH=/home/site/wwwroot
+
+# Start gunicorn with proper settings
+gunicorn --bind=0.0.0.0:8000 --timeout 600 --workers 4 wsgi:app 
